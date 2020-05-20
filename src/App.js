4742b1ffcf633e3f0821def7c3e2e53b7b473e6c
srkirve77 <41838493+srkirve77@users.noreply.github.com>
2020-05-21 03:06:20 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
 import  ShopPage from'./pages/shop/shop.component.jsx'
+import Header from '../src/components/header/header.component'
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -12,9 +13,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
+      <Switch>
       <Route exact path='/' component={Homepage}/>
       <Route exact path='/shop' component={ShopPage}/>
-
+      </Switch>
     </div>
   );
 }
