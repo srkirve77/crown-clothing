3f25314e549bc9641a1be4edb54432536cbd58e8
srkirve77 <41838493+srkirve77@users.noreply.github.com>
2020-05-22 01:40:47 +0530

    created sign up functionality which adds user in firebase database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from './pages/homepage/homepage.component'
 import  ShopPage from'./pages/shop/shop.component.jsx'
 import Header from '../src/components/header/header.component'
 import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth } from '../src/firebase/firebase.utils.js'
+import {auth, createUserProfileDocument } from '../src/firebase/firebase.utils.js'
 const HatsPage = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -22,10 +22,22 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null;
   componentDidMount() {
-    auth.onAuthStateChanged(user => 
-      {
-        this.setState({currentUser:user});  
-      } );
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+        if(userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+          userRef.onSnapshot(snapshot => {
+            this.setState({
+              currentUser : {
+                id :snapshot.id,
+                ...snapshot.data()
+              }
+            })
+            console.log(this.state);
+          })
+        }
+        else
+        this.setState({currentUser:userAuth});
+    })
   }
 
   componentWillUnmount() {
--- src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 import '../sign-in/sign-in.component'
 import SignIn from '../sign-in/sign-in.component';
+import  SignUp from  '../sign-up/sign-up.component'
 const SignInAndSignUpPage =() => (
     <div className='sign-in-and-sign-up'>
         <SignIn/>
+        <SignUp></SignUp>
     </div>
 );
 export default SignInAndSignUpPage;
