22ae69ee6158cd1499fc489fe12eb7a538f3e4e1
srkirve77 <41838493+srkirve77@users.noreply.github.com>
2020-05-23 04:37:45 +0530

    added cart item functionality to cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import CustomButton from '../custome-button/custom-button.component'
 import './collection-item.styles.scss'
-const CollectionItem = ( {id,name , price, imageUrl} ) => (
-    <div className='collection-item'>
+import {connect } from 'react-redux'
+import {addItem} from '../../redux/cart/cart.actions'
+const CollectionItem = ( {item,addItem} ) => {
+ const {id,name,price,imageUrl} = item;
+ return  ( <div className='collection-item'>
         <div className = 'image'
             style = {
                 {
@@ -14,6 +18,10 @@ const CollectionItem = ( {id,name , price, imageUrl} ) => (
             <span className = 'name'><h3>{name}</h3></span>
             <span className = 'price'><h3>{price}</h3></span>
         </div> 
-    </div>
-);
-export default CollectionItem;
+        <CustomButton onClick={() => addItem(item)} inverted>ADD TO CART</CustomButton>
+    </div>)
+};
+const mapDispatchToProps=dispatch => ({
+    addItem :item => dispatch(addItem(item))
+})
+export default connect(null,mapDispatchToProps)(CollectionItem);
